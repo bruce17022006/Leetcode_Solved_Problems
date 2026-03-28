a93aaa4168d0b2ad4adf57fa7ba9f5d21a307d22
Purushothaman V <bruceraji17022006@gmail.com>
2026-03-28 19:58:41 +0530

    Time: 340 ms (86.35%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,2 @@
+/* Write your PL/SQL query statement below */
+select m.machine_id as machine_id, round(avg(p.timestamp - m.timestamp),3) as processing_time from Activity m join Activity p on m.machine_id=p.machine_id and m.process_id=p.process_id where m.activity_type='start' and p.activity_type='end' group by m.machine_id;
